6a6b9d2009891700753b6dfa4d4cb7bbb3808c7b
= <j.mosavi2@gmail.com>
2022-08-03 14:28:05 +0430

    using variable to set product code

--- sample-packages/cnpr-patterns/index.js
+++ sample-packages/cnpr-patterns/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const got = require('got');
 const patternsUrl = "http://api.chapyar.com/message-pattern";
-const productCode = "1MMM111MMM11M";
+const productCode = "";
 const request = got.get(patternsUrl, {searchParams: {productCode : productCode}});
 request.then((response) => {
     fs.writeFile(__dirname + 'tempResult', response.body,
@@ -9,6 +9,6 @@ request.then((response) => {
             console.log('Finish Successfully');
         });
 });
-exports.printMsg = function () {
-    console.log('This is a sample package');
+exports.SetProductCode = function (productCode) {
+    this.productCode = productCode;
 }
