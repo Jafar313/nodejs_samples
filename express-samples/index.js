eacad04e8341f925533fa58c9ed23128f09dd078
= <j.mosavi2@gmail.com>
2022-08-06 15:25:17 +0430

    try using callback design pattern, to call modules

--- express-samples/index.js
+++ express-samples/index.js
@@ -1,69 +1,67 @@
 import express from 'express';
-import {appendFile, open, read, readFile, writeFile} from 'fs';
+import {appendFile, open, readFile, writeFile} from 'fs';
 import path from 'path';
 import {fileURLToPath} from 'url';
 
 
 const app = express();
+app.use(express.json());
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dbPath = `${__dirname}/people.db`;
-const people = {"people": []};
-let fileAlreadyRead = false;
+const db = {"people": []};
 
-app.use(express.json());
+//init db
+readFile(dbPath, (err, result) => {
+    if (err) {
+        appendFile(dbPath, JSON.stringify({"people": []}), () =>{
+            console.log("new db file generated");
+        })
+    } else {
+        let dataFile = JSON.parse(result.toString());
+        for (let i = 0; i < dataFile.people.length; i++) {
+            db.people.push(dataFile.people[i]);
+        }
+    }
+});
 
+//routes
 app.get("/", (request, response) =>{
     return response.redirect("/people");
 });
 
 app.get("/people", (request, response) =>{
-    if (fileAlreadyRead === false){
-        const populateData = new Promise((resolve, error) =>{
-            if (InitializeDb() === true){
-                resolve();
-            }
-            populateData.then(() =>{
-                fileAlreadyRead = true;
-            })
-        });
-
-
-
-    }
-    return response.send(JSON.parse(people.people));
+    return response.send(db.people);
 })
 
 app.post("/people", (request, response) =>
 {
-    let person = {Id: Math.floor((Math.random() * 100) + 1), FullName: request.body.name, Age: request.body.age};
+    let person = {FullName: request.body.name, Age: request.body.age};
     if (person.FullName !== undefined & person.Age !== undefined) {
-        if (fileAlreadyRead === true) {
-            people.people.push(person);
-            open(dbPath, () => {
-                writeFile(dbPath, JSON.stringify(people), (err) => {
-                    if (err != null) {
-                        console.log(err);
-                        return response.sendStatus(500);
-                    } else {
-                        return response.send(person);
-                    }
-                });
-            });
-        } else {
-
-        }
+        person.Id = Math.floor((Math.random() * 100) + 1);
+        db.people.push(person);
+        SaveChanges();
+        return response.send(person);
     } else {
         return response.sendStatus(400);
     }
-    
+
 });
 
 app.put("/people/:id", (request, response) =>{
-    if (people.people.length !== 0){
-        let id = request.params[0];
-        console.log(request.params[0]);
-        people.people.findIndex(id => id === people.people.Id);
+    if (db.people.length > 0){
+        let personIndex = db.people.findIndex(x => x.Id == request.params[0]);
+        if (personIndex > -1){
+            db.people.splice(personIndex, 1);
+            SaveChanges();
+        }
+        else{
+            return response.sendStatus(404);
+        }
+    }
+    else{
+        console.log("db is empty");
     }
 })
 
@@ -73,18 +71,10 @@ app.delete("/people/:id", (request, response) =>{
 
 app.listen(3000);
 
-function InitializeDb() {
-    readFile(dbPath, (err, result) => {
-        if (err) {
-            appendFile(dbPath, JSON.stringify({"people": []}), () => {
-            });
-        } else {
-            let dataFile = JSON.parse(result.toString());
-            for (let i = 0; i < dataFile.people.length; i++) {
-                people.people.push(dataFile.people[i]);
-            }
-        }
+function SaveChanges(){
+    open(dbPath, 'w', ()=>{
+        writeFile(dbPath, JSON.stringify(db), () =>{
+            console.log("Changes has been saved");
+        });
     });
-
-    return true;
 }
