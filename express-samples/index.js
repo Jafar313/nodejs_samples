f4151ca5cf4eafee1e81b2dd6f7d0df591a11583
= <j.mosavi2@gmail.com>
2022-08-06 15:02:04 +0430

    Improving reading data from file and insert new ones

--- express-samples/index.js
+++ express-samples/index.js
@@ -8,7 +8,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dbPath = `${__dirname}/people.db`;
-const people = [];
+const people = {"people": []};
+let fileAlreadyRead = false;
 
 app.use(express.json());
 
@@ -16,65 +17,74 @@ app.get("/", (request, response) =>{
     return response.redirect("/people");
 });
 
-app.get("/people", (request, response) => {
-    readFile(dbPath, (err, data) =>{
-        if (err !== null){
-            return response.sendStatus(404);
-        } else{
-            console.log(`data receieved is: ${data}`);
-            return response.send(data);
-        }        
-    })
-})
-        
-app.post("/add-new-person", (request, response) =>
-{
-    console.log(request.body);
-    let person = {FullName: request.body.name, Age: request.body.age};   
-    console.log("person array is: "+ JSON.stringify(person));
-    if (person.FullName !== undefined & person.Age !== undefined){
-        console.log('person object is not null');
-        readFile(dbPath, (err, data) => {
-            if (err != null){
-                console.log("cannot read db");
-                console.log(err);
-                 data = "{'people':[]}";
-                appendFile(dbPath, JSON.stringify(emptyData), () =>{
-                    console.log("new db generated and empty data inserted");
-                });
+app.get("/people", (request, response) =>{
+    if (fileAlreadyRead === false){
+        const populateData = new Promise((resolve, error) =>{
+            if (InitializeDb() === true){
+                resolve();
             }
-            else {
-                console.log(`received data is: ${data}`)
-                people.push(JSON.parse(data));
-            }
+            populateData.then(() =>{
+                fileAlreadyRead = true;
+            })
         });
-        open(dbPath, () => {
-            console.log("writing into db started...");
-            console.log("writing data is:" + JSON.stringify(people));
-            writeFile(dbPath, JSON.stringify(people), (err) => {
-                if (err != null){
-                    console.log(err);
-                }
-                else{
-                    console.log("person has been appended");
-                    return response.send(person);
-                }
+
+
+
+    }
+    return response.send(JSON.parse(people.people));
+})
+
+app.post("/people", (request, response) =>
+{
+    let person = {Id: Math.floor((Math.random() * 100) + 1), FullName: request.body.name, Age: request.body.age};
+    if (person.FullName !== undefined & person.Age !== undefined) {
+        if (fileAlreadyRead === true) {
+            people.people.push(person);
+            open(dbPath, () => {
+                writeFile(dbPath, JSON.stringify(people), (err) => {
+                    if (err != null) {
+                        console.log(err);
+                        return response.sendStatus(500);
+                    } else {
+                        return response.send(person);
+                    }
+                });
             });
-        });   
+        } else {
+
+        }
+    } else {
+        return response.sendStatus(400);
     }
-    else{
-        console.log('person object is null');
-        console.log(person);
-        return response.end();
-    }
+    
 });
 
-app.put("/remove/:id", (request, response) =>{
-
+app.put("/people/:id", (request, response) =>{
+    if (people.people.length !== 0){
+        let id = request.params[0];
+        console.log(request.params[0]);
+        people.people.findIndex(id => id === people.people.Id);
+    }
 })
 
-app.delete("/delete/:id", (request, response) =>{
+app.delete("/people/:id", (request, response) =>{
 
 })
 
 app.listen(3000);
+
+function InitializeDb() {
+    readFile(dbPath, (err, result) => {
+        if (err) {
+            appendFile(dbPath, JSON.stringify({"people": []}), () => {
+            });
+        } else {
+            let dataFile = JSON.parse(result.toString());
+            for (let i = 0; i < dataFile.people.length; i++) {
+                people.people.push(dataFile.people[i]);
+            }
+        }
+    });
+
+    return true;
+}
