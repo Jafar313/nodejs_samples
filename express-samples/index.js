79f21f1b230d99fcceca4306bc4785e67c5ce99c
= <j.mosavi2@gmail.com>
2022-08-09 11:54:49 +0430

    Completing CRUD operations and fix some bugs in saving changes into db

--- express-samples/index.js
+++ express-samples/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import {appendFile, open, readFile, writeFile} from 'fs';
+import {writeFile} from 'fs/promises';
+import {appendFile, readFile} from 'fs'
 import path from 'path';
 import {fileURLToPath} from 'url';
 
@@ -27,54 +28,70 @@ readFile(dbPath, (err, result) => {
 });
 
 //routes
-app.get("/", (request, response) =>{
-    return response.redirect("/people");
-});
-
 app.get("/people", (request, response) =>{
     return response.send(db.people);
 })
 
 app.post("/people", (request, response) =>
 {
-    let person = {FullName: request.body.name, Age: request.body.age};
-    if (person.FullName !== undefined & person.Age !== undefined) {
+    if (request.body.name !== undefined && request.body.age !== undefined) {
+        let person = {};
         person.Id = Math.floor((Math.random() * 100) + 1);
+        person.FullName = request.body.name;
+        person.Age = request.body.age
         db.people.push(person);
-        SaveChanges();
-        return response.send(person);
+        console.log('before insert to file')
+        SaveChanges().then(() =>{
+            console.log('after insert to file')
+            return response.send(person);
+        });
+
     } else {
         return response.sendStatus(400);
     }
 
 });
 
-app.put("/people/:id", (request, response) =>{
-    if (db.people.length > 0){
-        let personIndex = db.people.findIndex(x => x.Id == request.params[0]);
+app.put("/people", (request, response) =>{
+    if (request.body.id !== undefined && request.body.id > 0){
+        let personIndex = db.people.findIndex(x => x.Id === request.body.id);
         if (personIndex > -1){
-            db.people.splice(personIndex, 1);
-            SaveChanges();
+            db.people[personIndex]["FullName"] = request.body.name;
+            db.people[personIndex]["Age"] = request.body.age;
+            SaveChanges().then(() => response.send(db.people[personIndex]));
         }
         else{
             return response.sendStatus(404);
         }
     }
     else{
-        console.log("db is empty");
+        return response.sendStatus(400);
     }
 })
 
 app.delete("/people/:id", (request, response) =>{
-
+    let personIndex = db.people.findIndex(x => x.Id === Number(request.params.id));
+    console.log('person index is:' + personIndex);
+    if (personIndex > -1){
+        let removedPerson = db.people.splice(personIndex, 1);
+        SaveChanges().then(() => {
+            response.send(removedPerson);
+        });
+    }
+    else{
+        return response.sendStatus(404);
+    }
 })
 
 app.listen(3000);
 
-function SaveChanges(){
-    open(dbPath, 'w', ()=>{
-        writeFile(dbPath, JSON.stringify(db), () =>{
-            console.log("Changes has been saved");
-        });
-    });
+async function SaveChanges(){
+    try{
+        console.log('saving to db file started');
+        await writeFile(dbPath, JSON.stringify(db));
+        console.log('saving to db file finished');
+    }
+    catch (err){
+        console.log(err);
+    }
 }
